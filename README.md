b6883fc4aaf81bec62ed6344cd756b12b69d0198
Albin Groen <albin.groen@gmail.com>
2021-10-13 23:35:01 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# unique-pairs
+
+Given an array of strings, return an object where each field/value is unique
+
+## Installation
+
+    npm install unique-pairs
+
+## Usage
+
+```javascript
+import { getUniquePairs } from "unique-pairs"
+
+console.log(getUniquePairs(["John", "Johanna", "Jane"]))
+
+// Example output:
+
+{ John: 'Jane', Johanna: 'John', Jane: 'Johanna' }
+```
---
+++ index.ts
@@ -0,0 +1,35 @@
+const getRandomInt = (max: number) => {
+  return Math.floor(Math.random() * (max - 0 + 1) + 0);
+};
+
+export const getUniquePairs = (nodes: string[]) => {
+  let uniquePairs: Record<string, string | null> = {};
+  let success = false;
+
+  const getReceiver = (giver: string): string | null => {
+    const availableNodes = nodes.filter(
+      (node) => node !== giver && !Object.values(uniquePairs).includes(node)
+    );
+
+    return availableNodes[getRandomInt(availableNodes.length - 1)] ?? null;
+  };
+
+  while (!success) {
+    nodes.forEach((node) => {
+      if (!uniquePairs.node) {
+        const receiver = getReceiver(node);
+        uniquePairs[node] = receiver;
+      }
+    });
+
+    if (Object.values(uniquePairs).every(Boolean)) {
+      success = true;
+    } else {
+      uniquePairs = {};
+    }
+  }
+
+  return uniquePairs;
+};
+
+console.log(getUniquePairs(["John", "Johanna", "Jane"]));
